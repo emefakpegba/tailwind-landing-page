a53895dad1313b10316a303979b5c77eeb0dde19
Emefa Kpegba <globalepub@gmail.com>
2025-10-07 12:48:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Prices from "./components/Prices";
+import Newsletter from "./components/Newsletter";
+import Testimonials from "./components/Testimonials";
+import Team from "./components/Team";
+import Contact from "./components/Contact";
+//import Mode from "./components/Mode";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Prices />
+      <Testimonials />
+      <Newsletter />
+      <Team />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mode.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//Add dark and light mode code here
+
+function Mode() {
+  return (
+    <div>Mode</div>
+  )
+}
+
+export default Mode
