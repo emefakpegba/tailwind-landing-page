d234251dbe4db09f048562cc0b3f67fb1e3b9e2f
Emefa Kpegba <emefakpegba@gmail.com>
2025-10-09 17:23:41 +0000

    svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,11 +70,7 @@ function Navbar() {
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Datafolia</span>
-                <img
-                  alt=""
-                  src="./src/assets/create-react-app.svg"
-                  className="h-8 w-auto"
-                />
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-database-icon lucide-database"><ellipse cx="12" cy="5" rx="9" ry="3"/><path d="M3 5V19A9 3 0 0 0 21 19V5"/><path d="M3 12A9 3 0 0 0 21 12"/></svg>
               </a>
               <button
                 type="button"
