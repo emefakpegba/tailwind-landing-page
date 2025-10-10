7cfae24e5bca494d7dddd843a8635c09820b625e
Emefa Kpegba <emefakpegba@gmail.com>
2025-10-10 21:31:32 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Newsletter from "./components/Newsletter";
 import Testimonials from "./components/Testimonials";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
+import Banner from "./components/Banner";
 //import Mode from "./components/Mode";
 
 function App() {
